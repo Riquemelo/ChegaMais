db2f3377aeeecd96f71586674057dda9f40eafb3
Henrique Melo <riquemelo98@gmail.com>
2018-03-28 21:42:32 -0300

    Intro adicionada + alguns recursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+//import { GoogleMaps } from '@ionic-native/google-maps';
+
 
 import { ProfilePage } from '../pages/profile/profile';
 import { FeedPage } from '../pages/feed/feed';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { IntroPage } from '../pages/intro/intro';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +21,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ProfilePage,
     FeedPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    IntroPage
   ],
   imports: [
     BrowserModule,
@@ -29,11 +34,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ProfilePage,
     FeedPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    IntroPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    //GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {  IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  slides = [
+    {
+      title: "<img id='title-intro' src='assets/imgs/logotipo1.png'> ",
+      description: "Procurando algum evento do seu interesse? Veio ao lugar certo ",
+      image: "assets/imgs/city.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/img/ica-slidebox-img-2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/img/ica-slidebox-img-3.png",
+    }
+  ];
+}
